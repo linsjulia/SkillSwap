0699ce0deaf16baa9b626f065c5743027c08db6b
julia <julia.silva1490@etec.sp.gov.br>
2024-09-11 17:19:58 -0300

    erro icon

---
+++ pags_atuais/splashScreen.tsx
@@ -0,0 +1,48 @@
+import { Text, SafeAreaView, StyleSheet, Image } from 'react-native';
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        Skill Swap
+      </Text>
+    
+      <Image style={styles.imagem} source = {require('./assets/logo.png')}>
+
+      </Image>
+  
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    padding: 8,
+
+  },
+
+  imagem: {
+    width: 250,
+    height: 250,
+    bottom: 90,
+  },
+
+
+  paragraph: {
+    margin: 25,
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#f0f8ff',
+    position: 'relative',
+    top: 300,
+    textShadowColor: 'rgba(151, 32, 255, 9.0)', 
+    textShadowOffset: { width: 0, height: 0 }, 
+    textShadowRadius: 40,
+  },
+});
