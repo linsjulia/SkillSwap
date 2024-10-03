b0356277a9c4ccf91df28a1900bba18b815e2525
nicole <nicole.goncalves6@etec.sp.gov.br>
2024-10-03 16:33:39 -0300

    router checkmark

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -128,7 +128,21 @@ export default function RootLayout() {
           },
         }}
       />
-     
+      <Stack.Screen
+        name="checkmark"
+        options={{
+          title: "Skill Swap",
+          headerStyle: {
+            backgroundColor: "#212121",
+          },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontFamily: "Inter",
+            fontWeight: "bold",
+          },
+        }}
+      />
+      
     </Stack>
   );
 }
--- src/app/checkmark.tsx
+++ src/app/checkmark.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
+import { router } from "expo-router";
 
 export default function ConfirmationScreen() {
   return (
