c345534f728a76f9765c1d64d7c0b42cd2429758
nicole <nicole.goncalves6@etec.sp.gov.br>
2024-10-28 20:26:41 -0300

    Arrumando rota

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -191,6 +191,21 @@ export default function RootLayout() {
           },
         }}
       />
+      <Stack.Screen
+        name="resetPassword"
+        options={{
+          title: "Skill Swap",
+          headerStyle: {
+            backgroundColor: "#212121",
+          },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontFamily: "Inter",
+            fontWeight: "bold",
+          },
+        }}
+      />
+      
       
     </Stack>
 
